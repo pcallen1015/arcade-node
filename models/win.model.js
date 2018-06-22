f20963904f33f730a26a9d5371b3d767403e7469
Colin Allen <colallen@cisco.com>
2018-06-21 18:57:37 -0700

    Removed all win-related definitions and functionality.

--- models/win.model.js
+++ models/win.model.js
@@ -1,10 +1,4 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let WinSchema = new Schema({
-    player: { type: String, required: true },
-    game: { type: String, required: true },
-    playedOn: { type: Date, required: true }
-});
-
-mongoose.model('Win', WinSchema);
+// Win Model
--- routes/wins.routes.js
+++ routes/wins.routes.js
@@ -1,7 +1,5 @@
 const winsCtrl = require('../controllers/wins.controller');
 
 module.exports = (app) => {
-    app.route('/wins')
-        .get(winsCtrl.list)
-        .post(winsCtrl.create);
+    // Win Routes
 }
